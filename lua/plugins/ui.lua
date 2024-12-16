f464404cec1b5d2f40470e75502a0f508a3e015d
ItouRui <itourui@MacbookPro-14inch-M4-Max.local>
2024-12-16 13:02:48 +0900

    feat(ui): configure toggleterm plugin with custom options
    - Added key mappings for terminal mode to enhance usability.
    - Configured terminal size, open mapping, and other options.
    - Set terminal direction to float with curved border and winblend.
    - Enabled scrollback with a buffer size of 10000.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -53,6 +53,39 @@ return {
 		end,
 	},
 
+	-- toggleterm
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = function(_, opts)
+			vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+			vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+			vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+
+			-- scroll
+			vim.keymap.set("t", "<S-PageUp>", [[<C-\><C-n><C-b>]], opts)
+			vim.keymap.set("t", "<S-PageDown>", [[<C-\><C-n><C-f>]], opts)
+
+			opts.size = 100
+			opts.open_mapping = [[<c-\>]]
+			opts.hide_numbers = true
+			opts.shade_filetypes = {}
+			opts.shade_terminals = true
+			opts.shading_factor = 2
+			opts.start_in_insert = true
+			opts.insert_mappings = true
+			opts.persist_size = true
+			opts.direction = "float"
+			opts.close_on_exit = true
+			opts.shell = vim.o.shell
+			opts.float_opts = {
+				border = "curved",
+				winblend = 3,
+			}
+			opts.scrollback = 10000
+		end,
+	},
+
 	{
 		"rcarriga/nvim-notify",
 		opts = {
